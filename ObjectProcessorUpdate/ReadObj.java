f1e6c7ac45c97dcd2af1f47169026fcfd69a1aaf
Lingbo Tang <lingbo19.tang@gmail.com>
2015-02-09 19:44:16 -0700

    Update ReadObj.java

--- ObjectProcessorUpdate/ReadObj.java
+++ ObjectProcessorUpdate/ReadObj.java
@@ -1,9 +1,7 @@
-import java.applet.Applet;    
+import java.applet.Applet;      
 import java.awt.BorderLayout;
 import java.awt.Button;
-import java.awt.Container;
 import java.awt.GraphicsConfiguration;
-import java.awt.Image;
 import java.awt.Panel;
 import java.awt.Scrollbar;
 import java.awt.event.ActionEvent;
@@ -14,41 +12,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URI;
-
 import javax.swing.JPanel;
 import javax.media.j3d.Appearance;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.DirectionalLight;
-import javax.media.j3d.Material;
-import javax.media.j3d.Node;
 import javax.media.j3d.PolygonAttributes;
 import javax.media.j3d.Shape3D;
 import javax.media.j3d.Texture;
 import javax.media.j3d.TextureAttributes;
 import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
-
-import java.net.URL;
-
+import javax.media.j3d.TransparencyAttributes;
 import javax.vecmath.Color3f;
 import javax.vecmath.Color4f;
 import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
-
 import com.sun.j3d.loaders.IncorrectFormatException;
 import com.sun.j3d.loaders.ParsingErrorException;
 import com.sun.j3d.loaders.Scene;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.utils.applet.MainFrame;
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
-import com.sun.j3d.utils.geometry.ColorCube;
-import com.sun.j3d.utils.geometry.Primitive;
 import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.universe.SimpleUniverse;
+
+
+// At first create 3 scenegroup move it let 2 untransperent when click the 3D mode
+// Remove the Textured one and then display the 2.
+// For specific comments, just look at my lab 3.
 
 public class ReadObj extends Applet implements AdjustmentListener, ActionListener {
 
@@ -58,128 +53,86 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
 	 */
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * File path, Simple Universe and BranchGroup
-	 * are the most important part of the program.
-	 */
-
 	private SimpleUniverse u = null;
 	private static String objpath="";
 	private static String texturepath ="";
 	private ObjectFile objfile = new ObjectFile(ObjectFile.RESIZE);
 	private PolygonAttributes pa;
 	private Scene s = null;
+	private TransformGroup trans = new TransformGroup();
 	private Texture texture;
 	private TextureAttributes texAttr;
-	private Appearance appearance;
+	private Appearance appearance,appearance2;
+	private BranchGroup template2,template;
+	private static TransparencyAttributes TA;
 	
-	/**
-	 * UI part
-	 */
 	private Scrollbar   sb;
     private Scrollbar	sb2;
     private Scrollbar	sb3;
-    private Transform3D sliderXform;
+    private Transform3D trans3d,trans3d2;
     private Vector3f    sliderVector;
-    private TransformGroup sliderTrans;
+    private TransformGroup sliderTrans,sliderTrans2;
     private Panel p = new Panel();
     private Button points = new Button("Points");
     private Button lines = new Button("Line");
     private Button mesh = new Button("Mesh");
     private Button skin = new Button("Skin");
     private Button HighDmode = new Button("3DMode");
-    
-    
-    /**
-     * Texture file
-     */
-    private URL url;
-    private String baseProto = "";
-    private String baseHost = "";
-    private String baseFile = "";
+    private int is3D = 0;
 	
-    /**
-     * Texture color
-     */
     private Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
     private Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
-    private Color3f red = new Color3f(0.7f, .15f, .15f);
+    private Color3f red = new Color3f(1.0f, 0.0f, 0.0f);
+    private Color3f blue = new Color3f(0.0f, 0.0f, 1.0f);
+    private ColoringAttributes redT = new ColoringAttributes(red,ColoringAttributes.NICEST);
+    private ColoringAttributes blueT = new ColoringAttributes(blue,ColoringAttributes.NICEST);
+    private static float x = 0.0f, y=0.0f, z=0.0f;
+    
+    
     
     public ReadObj() {
 		
     }
     
-	public BranchGroup createSceneGraph() {
+	public BranchGroup createSceneGraph(TransformGroup Ttrans, Transform3D HD, Appearance ap) {
 
-        // Create the root of the branch graph
+		// Initial the BranchGroup
         BranchGroup objRoot = new BranchGroup();
+        objRoot.addChild(Ttrans);
         
-        // Add the Slider transform to the Transform Group
-        sliderXform = new Transform3D();
-        sliderVector = new Vector3f();
-        sliderTrans = new TransformGroup(sliderXform);
-        sliderTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-        objRoot.addChild(sliderTrans);
-        
-        
-        // Create a transform group, so we can manipulate the object
-        TransformGroup trans = new TransformGroup();              
+        // Add basic transform group
+        trans = new TransformGroup();              
         trans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-        sliderTrans.addChild(trans);
+        Ttrans.addChild(trans);
         
-        // create a behavior to allow the user to move the object with the mouse
+        // Add Rotate mouse Group
         MouseRotate mr = new MouseRotate();
-        
-        // tell the behavior which transform Group it is operating on
         mr.setTransformGroup(trans);
-        
-        // create the bounds for rotate behavior (centered at the origin) 
         BoundingSphere bounds = new BoundingSphere(new Point3d(10.0,10.0,10.0), 300.0);
         mr.setSchedulingBounds(bounds);
-        
-        // add the Rotate Behavior to the root.(not the transformGroup
         objRoot.addChild(mr);
         
-        
-        // since the transfom in the transformGroup will be changing when we rotate the object
-        // we need to explicitly allow the transform to be read, and written.
         trans.setCapability(trans.ALLOW_TRANSFORM_READ);
         trans.setCapability(trans.ALLOW_TRANSFORM_WRITE);
         
-        
         //TextureLoader loader = new TextureLoader(texturepath,"RPG", new Container());
         
-        
-        //appearance.setMaterial(new Material(red, black, red, black, 1.0f));
-        //int primflags = Primitive.GENERATE_NORMALS + Primitive.GENERATE_TEXTURE_COORDS;
-        
+        // Add Scene Branchgroup
         Scene s = readScene(objpath);
-        
         BranchGroup scenegroup = s.getSceneGroup();
+        //Scene s2 = readScene(objpath);
+        //BranchGroup scenegroup2 = s2.getSceneGroup();
         
-        
-        
-        
-        //appearance.setTexture(null);
-        
-        // Create the polygon attributes to display only the wireframe
-        pa = new PolygonAttributes();
-        pa.setCapability(PolygonAttributes.ALLOW_MODE_WRITE);
-        pa.setPolygonMode(pa.POLYGON_FILL);
-        pa.setCullFace(pa.CULL_NONE);
-        
-        // set the polygon attributes
+        // Add Writbale polygon attributes 
         Shape3D Poly = (Shape3D)scenegroup.getChild(0);
         Poly.setCapability(Shape3D.ALLOW_APPEARANCE_WRITE);
-        appearance.setPolygonAttributes(pa);
+        ap.setPolygonAttributes(pa);
         Poly.setAppearance(appearance);
         
-        // Add it back to the transform group
         trans.addChild(scenegroup);
-         
-        // Have Java 3D perform optimizations on this scene graph.
-        // Create a red light that shines for 100m from the origin
-
+        //trans.addChild(scenegroup2);
+        
+        // Add Light
         Color3f light1Color = new Color3f(0.5f, 0.5f, 0.5f);
         Vector3f light1Direction = new Vector3f(2.0f, -5.0f, -8.0f);
         DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
@@ -192,9 +145,8 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
         return objRoot;
     }
 
+	// Read the OBJ file
 	public Scene readScene(String filename) {
-		
-		//Try to Load the file catch exceptions
         Scene s = null;
         try {
             s = objfile.load(filename);
@@ -211,52 +163,35 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
         return s;
 	}
 
-	
-	
-	
+	// Add just the coordinate translation to the X,Y and Z axis
 	public void adjustmentValueChanged(AdjustmentEvent e) {
-        sliderVector.set((float)sb2.getValue()/20.0f, (float)sb.getValue()/20.0f, (float)sb3.getValue()/10.0f);
-        sliderXform.setTranslation(sliderVector);
-        sliderTrans.setTransform(sliderXform);
+		x = (float)sb2.getValue()/20.0f;y=(float)sb.getValue()/20.0f;z = (float)sb3.getValue()/20.0f;
+        trans3d.setTranslation(new Vector3d(x,y,z));
+        trans3d2.setTranslation(new Vector3d(x+0.05f,y,z));
+        sliderTrans.setTransform(trans3d);
+        sliderTrans2.setTransform(trans3d2);
     }
-	
 	
 	public void init() {
 		
-		// Initialize the 3D Canvas and the simple universe first
-		
+		// Initialize the canvas and the universe
         setLayout(new BorderLayout());
         GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
-
         Canvas3D c = new Canvas3D(config);
         add("Center", c);
-        // Add the vertical scroll bar to the east to addjust the
-        // object position on the Y direction (Vertical)
         
+        // Add Scrollbar to adjust the coordinates
         sb = new Scrollbar(Scrollbar.VERTICAL, 0, 1, -10, 10);
         sb.addAdjustmentListener(this);
         add("East", sb);
-        
-        
-        // Add the vertical scroll bar to the south to addjust the
-        // object position on the X direction (Horizontal).
         sb2 = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, -10, 10);
         sb2.addAdjustmentListener(this);
         add("South", sb2);
-        
-        
-        // Add the vertical scroll bar to the north to addjust the
-        // object scale on the depth direction.
         sb3 = new Scrollbar(Scrollbar.HORIZONTAL, 0, 1, -10, 10);
         sb3.addAdjustmentListener(this);
         add("North", sb3);
-        
-        
-        // Add three button in a panel to swtich the display mode
-        // "points" to display the point cloud
-        // "lines" to display the lines
-        // "mesh" to display the object covered with mesh
-        //p =new Panel();
+
+        // Add button to switch the mode
         p.add(points);
         add("West",p);
         points.addActionListener(this);
@@ -273,48 +208,67 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
         add("West",p);
         HighDmode.addActionListener(this);
 
+        // Add texture to the appearance
         texture = new TextureLoader(texturepath, this).getTexture();
-        //texture = loader.getTexture();
         texture.setBoundaryModeS(Texture.WRAP);
         texture.setBoundaryModeT(Texture.WRAP);
         texture.setBoundaryColor(new Color4f(0.0f, 1.0f, 0.0f, 0.0f));
         texAttr = new TextureAttributes();
         texAttr.setTextureMode(TextureAttributes.MODULATE);
-        // Create a new Appearance
         appearance = new Appearance();
-        
-        // Specify that we ARE using a texture
-        //appearance.setCapability(Appearance.ALLOW_TEXTURE_ATTRIBUTES_WRITE);
-        
-
         appearance.setCapability(Appearance.ALLOW_TEXTURE_WRITE);
-        //appearance.setTexture(null);
-
+        appearance.setCapability(Appearance.ALLOW_POLYGON_ATTRIBUTES_WRITE);
+        appearance.setCapability(Appearance.ALLOW_COLORING_ATTRIBUTES_WRITE);
         appearance.setTextureAttributes(texAttr);
-        // Create a simple scene and attach it to the virtual universe
-        BranchGroup template = createSceneGraph();
         
-
+        appearance2 = new Appearance();
+        appearance2.setCapability(Appearance.ALLOW_TEXTURE_WRITE);
+        appearance2.setCapability(Appearance.ALLOW_POLYGON_ATTRIBUTES_WRITE);
+        appearance2.setCapability(Appearance.ALLOW_COLORING_ATTRIBUTES_WRITE);
+        appearance2.setColoringAttributes(blueT);
+        
+        // Add TransparencyAttributes
+        TA = new TransparencyAttributes(TransparencyAttributes.NICEST, 0.4f);
+        appearance.setTransparencyAttributes(TA);
+        appearance2.setTransparencyAttributes(TA);
+        
+        // Add Polygon attributes
+        pa = new PolygonAttributes();
+        pa.setCapability(PolygonAttributes.ALLOW_MODE_WRITE);
+        pa.setCullFace(pa.CULL_NONE);
+        
+        // Add slider transform group
+        
+        trans3d = new Transform3D();
+        trans3d.setTranslation(new Vector3d(x,y,z));
+        trans3d.setScale(0.5);
+        sliderTrans = new TransformGroup(trans3d);
+        sliderTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        
+        trans3d2 = new Transform3D();
+        trans3d2.setTranslation(new Vector3d(x+0.05f,y,z));
+        trans3d.setScale(0.5);
+        sliderTrans2 = new TransformGroup(trans3d2);
+        sliderTrans2.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+        
+        // Create two different template
+        
+        template2 = createSceneGraph(sliderTrans2,trans3d2,appearance2);       
+        template = createSceneGraph(sliderTrans,trans3d,appearance);
+ 
         u = new SimpleUniverse(c);
-
-        // This will move the ViewPlatform back a bit so the
-        // objects in the scene can be viewed.
         u.getViewingPlatform().setNominalViewingTransform();
-
-        // add the objects to the universe
         u.addBranchGraph(template);
         
         
     }
 	
+	// Destroy the 3D scene when exit
 	public void destroy() {
 	    u.removeAllLocales();
 	}
 	
-	/*
-     * The following allows the code to be run as an application
-     * as well as an applet
-     */
+	// Pipeline to pass in the OBJ file path and texture file path
 	public static void main(String[] args) {
     	System.out.print("Enter obj address: ");
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -337,6 +291,7 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
         new MainFrame(new ReadObj(), 512, 512);
     }
 
+	// Switch mode listener
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == points)
@@ -361,8 +316,8 @@ public class ReadObj extends Applet implements AdjustmentListener, ActionListene
 			}
 		}
 		else if (e.getSource() == HighDmode) {
-			
+			trans.removeChild(0);
+			appearance.setTexture(null);			
 		}
 	}
-	
 }
